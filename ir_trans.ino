7582aedf81b403f1c011f0f542dd0f70ed99a62a
sabinM1 <sabinM1@users.noreply.github.com>
2020-04-26 00:54:07 +0300

    Transmite coduri IR la un anumit timp

---
+++ ir_trans.ino
@@ -0,0 +1,54 @@
+#include <Time.h>
+#include <TimeLib.h>
+#include <IRremote.h>
+
+IRsend irsend;
+
+void setup() {
+  Serial.begin(9600); // initialize Serial communication
+  //while(!Serial) ;    // wait for serial port to connect.
+
+  // set the current time to 14:27:00, December 14th, 2015
+  setTime(20, 8, 00, 24, 4, 2020);
+}
+
+void loop() {
+  Serial.print("Time now is: ");
+  print2digits(hour());
+  Serial.print(":");
+  print2digits(minute());
+  Serial.print(":");
+  print2digits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print("/");
+  Serial.print(month());
+  Serial.print("/");
+  Serial.print(year());
+  Serial.println();
+
+  if ((hour())==23 && (minute())==1 && ((second())==1 or (second())==2))
+    irsend.sendNEC(0xFFB04F, 32);
+  if ((hour())==2 && (minute())==1 && ((second())==1 or (second())==2))
+    irsend.sendNEC(0xFFF807, 32);
+  delay(1000);
+}
+
+void print2digits(int number) {
+  if (number >= 0 && number < 10) {
+    Serial.print('0');
+  }
+  Serial.print(number);
+}
+
+
+
+//    irsend.sendNEC(0xFFB04F, 32); //sau 32 idk sau 20
+//    delay(500);
+//    irsend.sendNEC(0xFFF807, 32);
+//    delay(500);
+
+/*
+ * FFB04F - ON 
+ * FFF807 - OFF 
+ */
